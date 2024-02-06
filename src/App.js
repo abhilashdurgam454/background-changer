e8b1b3b501b2729bf0cdd193a92cf153521ddc15
abhilashdurgam454 <durgamabhilash15@gmail.com>
2024-02-05 17:12:36 -0800

    changed

--- src/App.js
+++ src/App.js
@@ -10,46 +10,46 @@ export default function App() {
         <div className="flex justify-around pt-5">
           <button
             onClick={() => setColor("red")}
-            className="text-white border-2 p-1 w-32 rounded-md text-center  bg-slate-500"
+            className="text-white border-2 p-1 w-32 rounded-md text-center  bg-red-500"
           >
             RED
           </button>
 
           <button
             onClick={() => setColor("green")}
-            className="text-white bg-pink border-2 p-1 w-32 rounded-md text-center bg-slate-500"
+            className="text-white bg-pink border-2 p-1 w-32 rounded-md text-center bg-green-800"
           >
             GREEN
           </button>
 
           <button
             onClick={() => setColor("black")}
-            className="text-white border-2 p-1  w-32 rounded-md text-center  bg-slate-500"
+            className="text-white border-2 p-1  w-32 rounded-md text-center  bg-black"
           >
             BLACK
           </button>
 
           <button
             onClick={() => setColor("pink")}
-            className="text-white border-2 p-1 w-32 rounded-md text-center  bg-slate-500"
+            className="text-white border-2 p-1 w-32 rounded-md text-center  bg-pink-500"
           >
             PINK
           </button>
           <button
             onClick={() => setColor("orange")}
-            className="text-white border-2 p-1 w-32 rounded-md text-center  bg-slate-500"
+            className="text-white border-2 p-1 w-32 rounded-md text-center  bg-orange-500"
           >
             ORANGE
           </button>
           <button
             onClickCapture={() => setColor("yellow")}
-            className="text-white border-2 p-1 w-32 rounded-md text-center  bg-slate-500"
+            className="text-white border-2 p-1 w-32 rounded-md text-center  bg-yellow-500"
           >
             YELLOW
           </button>
           <button
             onClick={() => setColor("blue")}
-            className="text-white border-2 p-1 w-32 rounded-md text-center  bg-slate-500"
+            className="text-white border-2 p-1 w-32 rounded-md text-center  bg-blue-500"
           >
             BLUE
           </button>
