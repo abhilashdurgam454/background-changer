71f2ef71d54263c995f65916fda84a252a797b64
abhilashdurgam454 <durgamabhilash15@gmail.com>
2024-02-05 17:07:34 -0800

    firstcommit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,62 @@
-import React from 'react';
-import './style.css';
+import React, { useState } from "react";
+import "./style.css";
 
 export default function App() {
+  const [color, setColor] = useState("white");
+
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <div className="w-full h-screen" style={{ backgroundColor: color }}>
+        <div className="flex justify-around pt-5">
+          <button
+            onClick={() => setColor("red")}
+            className="text-white border-2 p-1 w-32 rounded-md text-center  bg-slate-500"
+          >
+            RED
+          </button>
+
+          <button
+            onClick={() => setColor("green")}
+            className="text-white bg-pink border-2 p-1 w-32 rounded-md text-center bg-slate-500"
+          >
+            GREEN
+          </button>
+
+          <button
+            onClick={() => setColor("black")}
+            className="text-white border-2 p-1  w-32 rounded-md text-center  bg-slate-500"
+          >
+            BLACK
+          </button>
+
+          <button
+            onClick={() => setColor("pink")}
+            className="text-white border-2 p-1 w-32 rounded-md text-center  bg-slate-500"
+          >
+            PINK
+          </button>
+          <button
+            onClick={() => setColor("orange")}
+            className="text-white border-2 p-1 w-32 rounded-md text-center  bg-slate-500"
+          >
+            ORANGE
+          </button>
+          <button
+            onClickCapture={() => setColor("yellow")}
+            className="text-white border-2 p-1 w-32 rounded-md text-center  bg-slate-500"
+          >
+            YELLOW
+          </button>
+          <button
+            onClick={() => setColor("blue")}
+            className="text-white border-2 p-1 w-32 rounded-md text-center  bg-slate-500"
+          >
+            BLUE
+          </button>
+
+        
+        </div>
+      </div>
     </div>
   );
 }
